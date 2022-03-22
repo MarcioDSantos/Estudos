babf8badb50ee152d5e397c9de5eceea8cc060ae
marciodsantos <marcioluisborgesds@gmail.com>
2022-03-21 21:46:42 -0300

    Exercício : Conversor de binário para decimal.

---
+++ src/Binário.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Binário
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        //Declaração de variáveis
+        int binário, decimal = 0, numero, potencial = 0;
+
+        System.out.println("** Código conversor de números binários para decimais. **");
+        System.out.println();
+        System.out.print("Informe um número Binário ( apenas 0's e 1's): ");
+        binário = input.nextInt();
+
+        while (binário > 0)
+        {
+            numero= binário%10;
+            decimal += numero*Math.pow(2,potencial);
+            binário = binário/10;
+            potencial++;
+        }
+       System.out.println("\nO binário informado resulta no decimal " + decimal + ".");
+    }
+}
+
